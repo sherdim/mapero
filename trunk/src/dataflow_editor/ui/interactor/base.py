f328c3a56428aef1592187113d7de80717e3ceaf
zojeda <zojeda@59f20a64-cb33-0410-a0f0-31a46714caf9>
2007-07-27 14:28:29 +0000

    task 3 - Incomplete : add mouse and keyword interactors http://code.google.com/p/mapero/issues/detail?id=3

---
+++ trunk/src/dataflow_editor/ui/interactor/base.py
@@ -0,0 +1,12 @@
+
+
+from dataflow_editor.config import Config
+
+class BaseInteractor(object):
+    def __init__(self, document, view, config_name):
+	self.document = document
+	self.view = view
+	self.config = Config().getConfig(config_name);
+
+    def on_event(event):
+        pass
