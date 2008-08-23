f940602d66d3df6e3747eec0a1d8ec294143dc1c
zojeda <zojeda@59f20a64-cb33-0410-a0f0-31a46714caf9>
2008-08-23 15:49:38 +0000

    working on persistence

--- src/mapero/core/data_type.py
+++ src/mapero/core/data_type.py
@@ -2,10 +2,10 @@
 # License: new BSD Style.
 
 
-from enthought.traits.api import HasTraits, Type, TraitType
+from enthought.traits.api import HasTraits, Type, TraitType, Any
 
 class DataType(HasTraits):
-    type = Type( TraitType, allow_none = False )
+    type = Any( transient=True ) #Type( TraitType, allow_none = False) persistence problems with it
     
     def is_compatible_with(self, other_type):
         return issubclass(self.type, other_type.type)
